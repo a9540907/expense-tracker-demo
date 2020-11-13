b1a82595cf6095f7b1769fe4687bb9ff2dcfaa30
stan_wang <a7569209@gmail.com>
2020-11-14 00:09:10 +0800

    modify recordSeeder

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,30 +1,42 @@
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const mongoose = require('mongoose')
 const Record = require('../record')
 const db = require('../../config/mongoose')
+const User = require('../user')
+const recordList = require('../../recordList.json').results
+
+const SEED_USER = {
+  name: 'test',
+  email: 'root@example.com',
+  password: '12345678'
+}
 
 db.once('open', () => {
-  console.log('mongodb connected!')
-  Record.create(
-    {
-      name: '租金',
-      category: '家居物業',
-      date: '2020-09-21',
-      amount: 6000,
-      icon: 'fas fa-home'
-    },
-    {
-      name: '晚餐',
-      category: '餐飲食品',
-      date: '2020-09-21',
-      amount: 120,
-      icon: 'fas fa-utensils'
-    },
-    {
-      name: '電影: 天能',
-      category: '休閒娛樂',
-      date: '2020-09-21',
-      amount: 120,
-      icon: 'fas fa-grin-beam'
-    }
-  )
+  bcrypt
+    .genSalt(10)
+    .then(salt => bcrypt.hash(SEED_USER.password, salt))
+    .then(hash => User.create({
+      name: SEED_USER.name,
+      email: SEED_USER.email,
+      password: hash
+    }))
+    .then(user => {
+
+      const userId = user._id
+      return Promise.all(Array.from(
+        { length: recordList.length },
+        (_, i) => {
+          const { name, category, icon, date, amount, merchant } = recordList[i]
+          return Record.create({ name, category, date, icon, amount, merchant, userId })
+        }
+      ))
+        .then(() => {
+          console.log('done')
+          process.exit()
+        })
+    })
 })
